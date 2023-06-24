20e56aab3b5a79bb52c7fdb1124b002631d9fbe9
Shin Sungyun <sinsungyun1630@gmail.com>
2023-06-25 01:16:12 +0900

    .env로 포트설정

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes/index");
+
 const connect = require("./schemas");
 connect();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json()); // POST body 안에 있는 데이터를 사용하기 위해 프로젝트 파일 전역에 json() 적용
 app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000, (req, res) => {
-  console.log("홈피에 오신걸 환영합니다.");
+  console.log(`${process.env.PORT || 3000} 포트에 접속 되었습니다.`);
 });
